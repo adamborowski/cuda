eac2deb7d31499e0205c0e96d282b0cf989efa1f
borovsky <fl.borovsky@gmail.com>
2015-05-30 14:16:49 +0200

    added device manager

--- src/device_manager.cuh
+++ src/device_manager.cuh
@@ -70,40 +70,62 @@ __device__ void updateBlockState(int i, numSamples, BlockState* state) {
 
 }
 
-__device__ void thread_A_iter(BlockState *state) {
-	//0. ile partii trzeba przeczytać z global (w ramach grupy A)
-	//1. ile elementów tablicy trzeba skopiować w sumie?
-	//2. ile iteracji wymagać będzie skopiowanie
-	//3. w konkretnej iteracji jaki wątek jaki element ma skopiować?
-}
-__device__ void thread_B_iter(BlockState *state) {
+__device__ void thread_A_iter(int i, const int localId, const BlockState *state) {
+	/*
+	 * 0. ile partii trzeba przeczytać z global (w ramach grupy A)
+	 * 1. ile elementów tablicy trzeba skopiować w sumie?
+	 * 2. ile iteracji wymagać będzie skopiowanie
+	 * 3. w konkretnej iteracji jaki wątek jaki element ma skopiować?
+	 */
+	//ile elementów trzeba skopiować w bloku w jednej iteracji?
+	const int numElementsToCopyByBlock = state->num_B_threads * state->partSize;
+	//ile wątków kopiuje?
+	const int numCopyingThreads = state->num_A_threads;
+	//ile elementów ma skopiować pojedyczny wątek grupy A? Tyle będzie iteracji?
+	const int numElementsToCopyByThread = divceil(numElementsToCopyByBlock, numCopyingThreads);
+	//wsaźnik elementu w iteracji
+	const int elementIndex;
+
+	for (int i = 0; i < numElementsToCopyByThread; i++) {
+		elementIndex = numCopyingThreads * i + localId;
+		pritnf("\nT:[%d %d] A:%d i:%d", blockIdx.x, threadIdx.x, localId, i);
+		__syncthreads();
+	}
 
 }
-__device__ void thread_C_iter(BlockState *state) {
+__device__ void thread_B_iter(const int i, const int localId, BlockState *state) {
+
+}
+__device__ void thread_C_iter(const int i, const int localId, BlockState *state) {
 
 }
 
 __global__ void kernel_manager(int numSamples, float *samples, AggrPointers *outPointers) {
 	extern __shared__ void shared[];
 	BlockState *state = (BlockState*) shared;
-	int blockIndex = blockIdx.x;
+	const int blockIndex = blockIdx.x;
+
 	if (threadIdx.x == 0) { //zainicjalizujmy stan
 		setupBlockState(state);
 	}
+
 //wątki są przydzielone do grup w kolejności A, C, B
-	int firstThreadInCGroupIndex = state->num_A_threads;
-	int firstThreadInBGroupIndex = firstThreadInCGroupIndex + state->num_B_threads;
+	const int firstThreadInCGroupIndex = state->num_A_threads;
+	const int firstThreadInBGroupIndex = firstThreadInCGroupIndex + state->num_B_threads;
+	const int localId_A = threadIdx.x;
+	const int localId_C = threadIdx.x - firstThreadInCGroupIndex;
+	const int localId_B = threadIdx.x - firstThreadInBGroupIndex;
 	const int numIterations = state->numPartsPerBlock + 2; //potok ma 3 elementy zatem trzeba dodać 2 iteracje kończące
 	for (int i = 0; i < numIterations; i++) { //ile będzie iteracji? Ile każdy blok wczyta porcji danych?
 
-		if (threadIdx.x < firstThreadInCGroupIndex) { //jest to wątek typu A
+		if (localId_B >= 0) { //jest to wątek typu B
 
 		}
-		else if (threadIdx.x < firstThreadInBGroupIndex) { //jest to wątek typu C
+		else if (localID_C >= 0) { //to jest wątek typu C
 
 		}
-		else { //jest to wątek typu B
-
+		else { //wątek typu A
+			thread_A_iter(i, localId_A, block);
 		}
 		__syncthreads();
 		if (threadIdx.x == 0) {
