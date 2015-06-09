7e31ac43ee10bb3ce0e95f47bb61dac1cee8cb9b
borovsky <fl.borovsky@gmail.com>
2015-06-09 16:18:43 +0200

    sync added

--- tester/tester.py
+++ tester/tester.py
@@ -1,5 +1,7 @@
 __author__ = 'adam'
 
+import os
+
 from registers import *
 
 criteria = {
@@ -15,11 +17,32 @@ mgr = OverflowManager([
     CyclicRegister(criteria["numBlocks"]),
     CyclicRegister(criteria["device"])
 ])
-
 while True:
     current = mgr.getArray()
     # <test execution>
-    print current
+    conf = {
+        "inputSize": current[0],
+        "numBThreads": current[1],
+        "numBlocks": current[2],
+        "device": current[3]
+    }
+
+    cmd = "Debug/CUDAProj 128 " \
+          "{0[numBThreads]} 128 {0[numBlocks]} {0[device]} {0[inputSize]}" \
+          " | grep -oPe \"(?<=process time: )(.*)(?= ms)\"".format(conf)
+    results = []
+    numTests = 10
+    avg = 0
+    # gather time results and calculate average
+    for i in range(1, numTests):
+        time = float(os.popen(cmd).read())
+        avg += time
+        results.append(str(time))
+    avg /= numTests
+
+    outputLine = [str(c) for c in current] + results + [str(avg)]
+
+    print "\t".join(outputLine)
 
     # </test execution>
     mgr.next()
